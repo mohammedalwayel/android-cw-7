9775d53e642145cb4b1082b85eafa17268141543
mohammedalwayel <mohammedalwayel168@gmail.com>
2020-10-03 18:46:13 +0300

    done

---
+++ pokimon/app/src/main/java/com/example/pokimon/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pokimon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<pokimon> x = new ArrayList<>();
+        pokimon p1 = new pokimon("strong",R.drawable.mpo,122,200,300);
+        pokimon p2 = new pokimon("good"R.drawable.wee,122,150,400);
+        pokimon p3 = new pokimon("ok",R.drawable.wrr,1150,180,500);
+         pokimon.add(p1);
+         pokimon.add(p2);
+         pokimon.add(p3);
+
+
+
+
+
+    }
+}
---
+++ pokimon/app/src/main/java/com/example/pokimon/pokimon.java
@@ -0,0 +1,51 @@
+package com.example.pokimon;
+
+public class pokimon {
+
+
+    public static Object add;
+    private String name ;
+     private int attack;
+      private int defende ;
+        private int total;
+
+    public pokimon(String name, int mpo1, int mpo, int i, int i1) {
+
+    }
+
+    public static void add(pokimon p1) {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefende() {
+        return defende;
+    }
+
+    public void setDefende(int defende) {
+        this.defende = defende;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
